281f116d577b45f7b8271f42a35aea5d5878ae89
Daniel Figueredo <danielfsa2007@gmail.com>
2026-05-05 16:50:15 -0300

    feat: initial project structure for debt control system

---
+++ src/projects/debtcontrol/DebtService.java
@@ -0,0 +1,17 @@
+package projects.debtcontrol;
+
+import java.util.Scanner;
+
+public class DebtService {
+    public void listDebts(String [] names, double [] values, int [] days, int count){
+        Scanner scanner = new Scanner(System.in);
+    }
+
+    public void totalDebts(double [] values, int count){
+
+    }
+
+    public void showCloseDebts(String[] names, double[] values, int[] days, int count){
+
+    }
+}
---
+++ src/projects/debtcontrol/DebtSystem.java
@@ -0,0 +1,16 @@
+package projects.debtcontrol;
+
+import java.util.Scanner;
+
+public class DebtSystem {
+    public static void main() {
+        Scanner scanner = new Scanner(System.in);
+        int option;
+
+        while(true)
+        {
+
+        }//end while
+    }
+
+}
